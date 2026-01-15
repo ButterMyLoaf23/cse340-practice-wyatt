6519a0fa5e8b32a6ecd86e86868fd52be2caaa83
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-01-14 17:07:07 -0700

    Introduction to Routing

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const name = process.env.NAME;
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "src/views/home.html"));
 });
 
-const PORT = 3000;
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get("/products", (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
