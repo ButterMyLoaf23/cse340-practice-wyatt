db6fa0707ec5dce12a23d16b4493c772ea35f1fe
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-02-17 15:12:40 -0700

    space

--- src/controllers/faculty/faculty.js
+++ src/controllers/faculty/faculty.js
@@ -11,7 +11,7 @@ const facultyListPage = async (req, res) => {
     res.render('faculty/list', {
         title: 'Faculty Directory',
         faculty,
-        currentSort: sortBy
+        currentSort: sortBy 
     });
 };
 
